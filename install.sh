ce5f026b48829fc8f145733e649c00888ef98c31
Motoki Nagano <moto.n.mc.475.221@gmail.com>
2016-01-01 14:33:25 +0900

    Fix symbolic link destination

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 ln -s ~/.rc_files/gemrc ~/.gemrc
 ln -s ~/.rc_files/gnuplot ~/.gnuplot
 ln -s ~/.rc_files/gitconfig ~/.gitconfig
-ln -s ~/.rc_files/.zshrc.orig ~/.zshrc
+ln -s ~/.rc_files/.zshrc.org ~/.zshrc
 git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
 mkdir -p .vim/bundle
 ln -s ~/.rc_files/vim/vimrc ~/.vim/vimrc
