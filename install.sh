926d9931e494a51a537e56a0bc8da466cd62d4c1
Motoki Nagano <moto.n.mc.475.221@gmail.com>
2016-01-01 12:48:40 +0900

    Add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+ln -s ~/.rc_files/gemrc ~/.gemrc
+ln -s ~/.rc_files/gnuplot ~/.gnuplot
+ln -s ~/.rc_files/gitconfig ~/.gitconfig
+ln -s ~/.rc_files/.zshrc.orig ~/.zshrc
+git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+mkdir -p .vim/bundle
+ln -s ~/.rc_files/vim/vimrc ~/.vim/vimrc
+ln -s ~/.rc_files/vim/vimrc.plugin ~/.vim/vimrc.plugin
+ln -s ~/.rc_files/vim/vimrc.local ~/.vim/vimrc.local
+
+exit 0
