8d2fd6fc6c117c3da56960c55c09c9b5745bee70
Motoki Nagano <motoki.n.310@gmail.com>
2019-10-23 10:16:37 +0900

    dotfilesの格納ディレクトリに依存しないよう変更

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
-ln -s ~/.rc_files/gemrc ~/.gemrc
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+ln -s {$SCRIPT_DIR}/gemrc ~/.gemrc
 echo "Successfully generating .gemrc"
-ln -s ~/.rc_files/gnuplot ~/.gnuplot
+
+ln -s {$SCRIPT_DIR}/gnuplot ~/.gnuplot
 echo "Successfully generating .gnuplot"
-ln -s ~/.rc_files/gitconfig ~/.gitconfig
+
+ln -s {$SCRIPT_DIR}/gitconfig ~/.gitconfig
 echo "Successfully generating .gitconfig"
-ln -s ~/.rc_files/zshrc.org ~/.zshrc
+
+ln -s {$SCRIPT_DIR}/zshrc.org ~/.zshrc
 echo "Successfully generating .zshrc"
+
 git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
 echo "Successfully cloning NeoBundle"
+
 mkdir -p ~/.vim/bundle
-ln -s ~/.rc_files/vim/vimrc ~/.vim/vimrc
-ln -s ~/.rc_files/vim/vimrc.plugin ~/.vim/vimrc.plugin
-ln -s ~/.rc_files/vim/vimrc.local ~/.vim/vimrc.local
+ln -s {$SCRIPT_DIR}/vim/vimrc ~/.vim/vimrc
+ln -s {$SCRIPT_DIR}/vim/vimrc.plugin ~/.vim/vimrc.plugin
+ln -s {$SCRIPT_DIR}/vim/vimrc.local ~/.vim/vimrc.local
 echo "Successfully generating vim settings"
 
 exit 0
