f6cca7f1569524a936119f432dab094efc38129d
Motoki Nagano <motoki.n.310@gmail.com>
2020-01-11 15:38:52 +0900

    シンボリックリンクの作成ディレクトリが{}で囲われる問題を解決

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 BASEDIR_RELATIVE=`dirname $0`
 SCRIPT_DIR=`cd $BASEDIR_RELATIVE; pwd`
 
-ln -s {$SCRIPT_DIR}/gemrc ~/.gemrc
+ln -s $SCRIPT_DIR/gemrc ~/.gemrc
 echo "Successfully generating .gemrc"
 
-ln -s {$SCRIPT_DIR}/gnuplot ~/.gnuplot
+ln -s $SCRIPT_DIR/gnuplot ~/.gnuplot
 echo "Successfully generating .gnuplot"
 
 echo "Generating .gitconfig"
@@ -17,19 +17,19 @@ echo "  email = $email" >> ~/.gitconfig.local
 /bin/echo -n "Input your name on Git repository: "
 read name
 echo "  name = $name" >> ~/.gitconfig.local
-ln -s {$SCRIPT_DIR}/gitconfig ~/.gitconfig
+ln -s $SCRIPT_DIR/gitconfig ~/.gitconfig
 echo "Successfully generating .gitconfig"
 
-ln -s {$SCRIPT_DIR}/zshrc.org ~/.zshrc
+ln -s $SCRIPT_DIR/zshrc.org ~/.zshrc
 echo "Successfully generating .zshrc"
 
 git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
 echo "Successfully cloning NeoBundle"
 
 mkdir -p ~/.vim/bundle
-ln -s {$SCRIPT_DIR}/vim/vimrc ~/.vim/vimrc
-ln -s {$SCRIPT_DIR}/vim/vimrc.plugin ~/.vim/vimrc.plugin
-ln -s {$SCRIPT_DIR}/vim/vimrc.local ~/.vim/vimrc.local
+ln -s $SCRIPT_DIR/vim/vimrc ~/.vim/vimrc
+ln -s $SCRIPT_DIR/vim/vimrc.plugin ~/.vim/vimrc.plugin
+ln -s $SCRIPT_DIR/vim/vimrc.local ~/.vim/vimrc.local
 echo "Successfully generating vim settings"
 
 exit 0
